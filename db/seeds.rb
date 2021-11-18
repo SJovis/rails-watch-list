3f5df4d4aa194a01f1cd5ac72fa8d3d5661fd7b4
Sergio Cerqueira <jovis.sergio@gmail.com>
2021-11-18 19:00:38 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+require 'json'
+
+response = RestClient.get "https://api.themoviedb.org/3/movie/top_rated?api_key=72e647f623a0a47d72034f2c64b4878e"
+repos = JSON.parse(response)['results'].first(15)
+repos.each do |movie|
+  Movie.create(title: movie['title'],
+               overview: movie['overview'],
+               poster_url: "https://image.tmdb.org/t/p/w400/#{movie['poster_path']}",
+               rating: movie['vote_average'])
+end
