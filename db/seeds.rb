13348c73a1190c665339706568dd15674e8eb316
Sergio Cerqueira <jovis.sergio@gmail.com>
2021-11-19 15:46:47 +0000

    enhanced seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
-require 'rest-client'
+require 'open-uri'
 require 'json'
 
-response = RestClient.get "https://api.themoviedb.org/3/movie/top_rated?api_key=72e647f623a0a47d72034f2c64b4878e"
-repos = JSON.parse(response)['results'].first(15)
-repos.each do |movie|
-  Movie.create(title: movie['title'],
-               overview: movie['overview'],
-               poster_url: "https://image.tmdb.org/t/p/w400/#{movie['poster_path']}",
-               rating: movie['vote_average'])
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
+puts "Movies created"
